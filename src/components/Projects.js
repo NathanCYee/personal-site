7bdf49d5025596fcb7b6a3e24429f056d699c971
Nathan Yee <yee.nathan.c@gmail.com>
2018-03-29 21:27:11 -0700

    added project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import Typist from 'react-typist';
+import ParallaxCard from 'react-parallax-card'
+import {Link} from 'react-router-dom'
 
 class Projects extends Component {
     render() {
@@ -16,6 +18,25 @@ class Projects extends Component {
                         </Typist>
                     </div>
                 </div>
+                <Link class="nav-item nav-link" to={"/personal-site/projects/train"}>
+                <ParallaxCard
+                    label='Virtual Toy Train'
+                    enableRotate
+                    enableTransform
+                    style={{ margin: '0 auto', width: 400, height: 275 }}>
+
+                    <img src={require('../purple-paper.jpg')} style={{ maxWidth: '100%', maxHeight: '100%' }} />
+
+                    <div /><div /><div />
+
+                    <img src={require('../7.png')} style={{ top: '20%', left: '30%', maxWidth: '90%', maxHeight: '90%'}} />
+
+                    <p style={{ fontSize: '3rem', fontWeight: 'bold', padding: '3rem' }}>
+                        Virtual Train
+                    </p>
+
+                </ParallaxCard>
+                </Link>
             </div>
         )
     }
--- src/layout/Content.js
+++ src/layout/Content.js
@@ -3,6 +3,7 @@ import Home from '../components/Home'
 import Resume from '../components/Resume'
 import Projects from '../components/Projects'
 import Contact from '../components/Contact'
+import Train from '../components/Train'
 import {Route} from 'react-router-dom';
 
 class Content extends Component {
@@ -14,6 +15,7 @@ class Content extends Component {
                 <Route path={"/personal-site/pages/resume"} component={Resume} />
                 <Route path={"/personal-site/pages/projects"} component={Projects} />
                 <Route path={"/personal-site/pages/contact"} component={Contact} />
+                <Route path={"/personal-site/projects/train"} component={Train} />
             </div>
         )
     }
